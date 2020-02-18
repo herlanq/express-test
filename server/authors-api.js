efd65f2ed5ed3ece54a7a24563e5680a6adc6029
herlanq <herlanq@xavier.edu>
2020-02-18 17:04:50 -0500

    updated

--- server/authors-api.js
+++ server/authors-api.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const authors = require('./authors');
+const books = require('./books');
 
 router.get('/', (req, res) => authors.getAll(req, res));
 router.get('/:id', (req, res) => authors.getById(req, res));
+router.get('/:id/books', (req, res) => books.getByAuthorId(req, res));
 
 module.exports = router;
---
+++ server/books.js
@@ -0,0 +1,30 @@
+const booklist = require('../data/books.json');
+
+const books = {
+  getAll: (req, res) => {
+    if (booklist) {
+      return res.status(200).send(booklist);
+    } else {
+      return res.sendStatus(404);
+    }
+  },
+
+  getById: (req, res) => {
+    const books = booklist.filter(a => a.AuthorId == req.params.id);
+    if (books && books.length) {
+      return res.status(200).send(books);
+    } else {
+      return res.sendStatus(404);
+    }
+  },
+  getByAuthorId: (req, res) => {
+    const books = booklist.filter(a => a.Id == req.params.id);
+    if (books && books.length) {
+      return res.status(200).send(books);
+    } else {
+      return res.sendStatus(404);
+    }
+  }
+};
+
+module.exports = books;
